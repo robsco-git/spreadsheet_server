3103ca2b107d1d736e7fb412e4533aa8ae5f4188
Robert Scott <rs@robsco.info>
2016-04-11 17:24:50 +0200

    Added GPLv3 license.

--- connection.py
+++ connection.py
@@ -1,3 +1,19 @@
+# This file is part of calc_server.
+
+# calc_server is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# calc_server is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with calc_server.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import logging
 from math import pow
 from com.sun.star.uno import RuntimeException
